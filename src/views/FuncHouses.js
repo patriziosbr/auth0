fa37b745ccf0b12af258da500444b4b7444010c2
patriziosbr <sbrozzi.patrizio@gmail.com>
2022-12-09 00:07:21 +0100

    check input ok todo return same name house

--- src/views/FuncHouses.js
+++ src/views/FuncHouses.js
@@ -9,9 +9,10 @@ import axios from 'axios';
 export const FuncHouses = () => {
   const { user } = useAuth0();
   //add and remove units
-  const [items, setArr] = useState([]);
+  let countUnits = "";
+  const [userUnit, setArr] = useState([]);
   let removeOne = false;
-  const removeMe = (name) =>  setArr(items.filter((el) => {
+  const removeMe = (name) =>  setArr(userUnit.filter((el) => {
     if(removeOne)
         return true;
     removeOne = el === name
@@ -19,25 +20,51 @@ export const FuncHouses = () => {
   } ));
   //END add and remove units
 
-  let countUnits = "";
-
   //define house name
   const [value, setValue] = useState('');
   const handleChange = (e) => setValue(e.target.value);
   //END define house name
 
+  let [errorUnit, setErrorUnit] = useState(0);
+  let [errorText, setErrorText] = useState(0);
+  let [errorInvalidChar, setErrorInvalidChar] = useState(0);
+  const createHouse = async () => {
+    //Check house units selected
+    if(userUnit.length < 1) {
+        errorUnit = setErrorUnit(1);
+        console.log("error select at leat one unit ");
+    }else {
+        errorUnit = setErrorUnit(0);
+    }
+    //Check house text length
+    if(value.length < 3) {
+        errorText = setErrorText(1);
+        console.log("error text too short");
+    } else {
+        errorText = setErrorText(0);
+        if(/[^0-9a-zA-Z]/.test(value)) {
+            errorInvalidChar = setErrorInvalidChar(1);
+            console.log("error errorInvalidChar");
+            // exit if the length is more than 3 but contain an invalid char (onlit alphnumeric accepted) 
+            return
+        } else {
+            errorInvalidChar = setErrorInvalidChar(0);
+        }
+    }
+    // show all the error at the same time and exit
+    if(userUnit.length === 0 || value.length < 3) 
+        return
 
-  const handleClick = async () => {
-    axios.post('http://localhost:8000/houses', 
-    {"name":value, "units":items})
-            .then( res => {
-                console.log('res axios senno è triste', res.data); //the pokemon
-            }).catch( err => {
-                console.log('axios error Post', err);
-                //alert in extremis
-    })
+    axios.post('http://localhost:8000/houses',
+        {"user_id":user.sub , "name":value, "units":userUnit })
+        .then( res => {
+            setArr([])
+            setValue('') //clear input after submit
+            console.log('bingo', res.data);
+        }).catch( err => {
+            console.log('axios error Post', err);
+        })
   };
-
 
 
   return (
@@ -50,12 +77,14 @@ export const FuncHouses = () => {
                 <Input type="text" 
                     value={value} 
                     onChange={handleChange} />
+                <h6 className={(errorText ? 'text-danger' : "d-none")}>House name at least 3 char</h6>
+                { errorInvalidChar ? <h6 className={'text-danger'}>Invalid char</h6> : ''}
             </Col>
             
             <Col xs="12" sm="6">
             {/* <div> debug 
-                <h5> items</h5>
-                {items.map((a, index) => (
+                <h5> userUnit</h5>
+                {userUnit.map((a, index) => (
                     <p key={index}>{a} - {index}</p>
                 ))}
             </div> */}
@@ -66,21 +95,22 @@ export const FuncHouses = () => {
                             <div>
                                 <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
                                 <Button type="button" onClick={() => removeMe(name)} >remove</Button>
-                                <span> {countUnits = (items.filter(item => 
+                                <span> {countUnits = (userUnit.filter(item => 
                                     item === name)).length} 
                                 </span>
-                                <Button type="button"  onClick={() => setArr([...items, name])}>add</Button>
+                                <Button type="button"  onClick={() => setArr([...userUnit, name])}>add</Button>
                             </div>
                         </li>
                         );
                     })}
                 </ul>
+                <h6 className={(errorUnit ? 'text-danger' : "d-none")}>Select dat least 1 unit</h6>
             </Col>
             <Col>
 
     <div>
 
-      <button onClick={handleClick}>Make request</button>
+      <button onClick={createHouse}>Make request</button>
 
     </div>
   
